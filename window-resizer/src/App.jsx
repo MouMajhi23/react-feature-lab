8e8a4f84f75f5ba1e02cf52d2b9e5c3b2ee8fa9b
MouMajhi <moumajhi@lenskart.com>
2026-03-31 14:24:15 +0530

    window resizer with help of useEffect hook

---
+++ window-resizer/src/App.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import './App.css'
+
+function App() {
+  const [width, setWidth] = useState(window.innerWidth);
+  const [height, setHeight] = useState(window.innerHeight);
+  useEffect(()=>{
+    const handleResize = ()=> {
+        setWidth(window.innerWidth)
+        setHeight(window.innerHeight)
+      }
+    window.addEventListener('resize', handleResize);
+    return () => {
+    window.removeEventListener('resize', handleResize);
+    }
+  },[])
+  return (
+    <>
+      <section id="center">
+        <h2>Window Resizer</h2>
+        <p>Width : {width}</p>
+        <p>Height : {height}</p>
+      </section>
+    </>
+  )
+}
+
+export default App
